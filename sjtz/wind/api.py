d9a2e6df237c23661d73a41de6a0a9a42b1c1788
Euler <2391299238@qq.com>
2025-03-20 16:44:32 +0800

    3.20 4

--- sjtz/wind/api.py
+++ sjtz/wind/api.py
@@ -9,7 +9,7 @@ dir = os.path.dirname(__file__)
 today = datetime.date.today()
 #print(today)
 w.start() # 默认命令超时时间为120秒，如需设置超时时间可以加入waitTime参数，例如waitTime=60,即设置命令超时时间为60秒 
-begin = today - datetime.timedelta(days=365)
+begin = today - datetime.timedelta(days=30)
 w.isconnected() # 判断WindPy是否已经登录成功
 data = w.edb(["S0114145","S0069669","S0031645","S0035819"],str(begin), end_date=str(today),Fill="Previous",usedf = True)[1]#comex黄金库存，comex黄金库存市值,黄金现货价（伦敦市场）#上海金交所黄金现货价
 z = np.around(np.multiply(data['S0114145'].values,data['S0069669'].values)/(10**9),decimals=2)
@@ -27,14 +27,17 @@ plt.rcParams['font.sans-serif'] = ['SimHei']
 plt.title('COMEX交易所黄金库存水平及市值')
 plt.savefig(os.path.join(dir,'plot.png'))
 
-prompt = f"""这是近一年COMEX交易所黄金库存量的数据：{data['S0114145']}
-这是近一年COMEX交易所黄金库存市值的数据(单位billian)：{z}
-这是近一年伦敦市场黄金现货价格数据：{data['S0031645']}
-这是近一年上海金交所黄金现货价格数据：{data['S0035819']}
+prompt = f"""这是近一个月COMEX交易所黄金库存量的数据：{data['S0114145']}
+这是近一个月COMEX交易所黄金库存市值的数据(单位billian)：{z}
+这是近一个月伦敦市场黄金现货价格数据：{data['S0031645']}
+这是近一个月上海金交所黄金现货价格数据：{data['S0035819']}
 任务：请根据上述结果分析判断近期是否存在套利策略。
 请使用Markdown格式回答，但不要用任何代码块（如 ```）包裹内容。
 """
+data.columns = ["comex黄金库存量","comex黄金库存市值","黄金现货价（伦敦市场）","上海金交所黄金现货价"]
+new_data = data.to_markdown()
 result = call_model(prompt)
-result+="![图](plot.png)"
+result+="\n"+"![图](plot.png)"
+result+="\n"+new_data
 with open(os.path.join(dir,"stragety.md"), "w", encoding="utf-8") as file:
             file.write(result)

d60d064f34e2de28bb91bec1455bf142e0e5b928
Euler <2391299238@qq.com>
2025-03-20 12:46:55 +0800

    3.20

--- sjtz/wind/api.py
+++ sjtz/wind/api.py
@@ -3,24 +3,36 @@ from call_model import call_model
 import datetime
 import os
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 dir = os.path.dirname(__file__)
 today = datetime.date.today()
 #print(today)
 w.start() # 默认命令超时时间为120秒，如需设置超时时间可以加入waitTime参数，例如waitTime=60,即设置命令超时时间为60秒 
 begin = today - datetime.timedelta(days=365)
 w.isconnected() # 判断WindPy是否已经登录成功
-data = w.edb("S0114145",str(begin), end_date=str(today),usedf = True)
-plt.plot(data[1].index.values,data[1])
+data = w.edb(["S0114145","S0069669"],str(begin), end_date=str(today),Fill="Previous",usedf = True)[1]#comex黄金库存，comex黄金库存市值
+z = np.around(np.multiply(data['S0114145'].values,data['S0069669'].values)/(10**9),decimals=2)
+fig, ax = plt.subplots()
+ax.plot(data.index.values, data['S0114145'].values, color='green')
+ax.set_ylabel('蛊司')
+ax.set_xlabel('时间')
+ax.spines['right'].set_visible(False) # ax右轴隐藏
+
+z_ax = ax.twinx() # 创建与轴群ax共享x轴的轴群z_ax
+z_ax.plot(data.index.values, z, color='blue')
+z_ax.set_ylabel('Billion')
+
 plt.rcParams['font.sans-serif'] = ['SimHei']
-plt.xlabel('时间')
-plt.ylabel('蛊司')
-plt.title('COMEX交易所黄金库存水平')
+plt.title('COMEX交易所黄金库存水平及市值')
 plt.savefig(os.path.join(dir,'plot.png'))
-prompt = f"""这是近一年COMEX交易所黄金库存量的数据：{data[1]}
-请根据上述结果分析判断近期是否存在套利策略。
+
+prompt = f"""这是近一年COMEX交易所黄金库存量的数据：{data['S0114145']}
+这是近一年COMEX交易所黄金库存市值的数据(单位billian)：{z}
+任务：请根据上述结果分析判断近期是否存在套利策略。
 请使用Markdown格式回答，但不要用任何代码块（如 ```）包裹内容。
 """
 result = call_model(prompt)
-result+="![示例图](plot.png)"
+result+="![图](plot.png)"
 with open(os.path.join(dir,"stragety.md"), "w", encoding="utf-8") as file:
             file.write(result)
